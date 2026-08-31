a591277bff382c93293ec6262dc543ff0463236e
AssoDePicche <samuelprado730@gmail.com>
2026-08-31 14:13:12 -0300

    feat: setup layout providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
-import type { Metadata } from "next";
+import Providers from '@/components/Layout/Providers';
 
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from 'next';
 
-import "./globals.css";
+import { Geist, Geist_Mono } from 'next/font/google';
+
+import './globals.css';
 
 const geistSans = Geist({
-    variable: "--font-geist-sans",
-    subsets: ["latin"],
+    variable: '--font-geist-sans',
+    subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-    variable: "--font-geist-mono",
-    subsets: ["latin"],
+    variable: '--font-geist-mono',
+    subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-    title: "GRUPDSI",
-    description: "",
+    title: 'GRUPDSI',
+    description: '',
 };
 
-export default function RootLayout({ children }: LayoutProps<"/">) {
+export default function RootLayout({ children }: LayoutProps<'/'>) {
     return (
-        <html lang="pt-BR" className={`${geistSans.variable} ${geistMono.variable}`}>
-            <body>{children}</body>
+        <html lang='pt-BR' className={`${geistSans.variable} ${geistMono.variable}`}>
+            <body>
+                <Providers>{children}</Providers>
+            </body>
         </html>
     );
 }
